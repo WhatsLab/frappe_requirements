67545a3ae8bd24174a258e0ba9f2f5f7ca464a11
Sami Alhelwah <sami.alhelwah@gmail.com>
2016-04-02 19:28:45 +0000

    fix doctype trigger name

--- frappe_requirements/frappe_requirements/doctype/doctype_requirement_trigger/doctype_requirement_trigger.py
+++ frappe_requirements/frappe_requirements/doctype/doctype_requirement_trigger/doctype_requirement_trigger.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class DocTypeRequirementTriger(Document):
+class DocTypeRequirementTrigger(Document):
 	pass
--- frappe_requirements/frappe_requirements/doctype/doctype_requirements/doctype_requirements.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016, Matajer.sa and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('DocType Requirements', {
-	refresh: function(frm) {
-
-	}
-});
